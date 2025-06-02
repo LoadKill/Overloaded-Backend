1ef57dc28e6e537cc13ef772fdf3d8500ace458b
Kim-Hui-Dong <129731833+huidong97@users.noreply.github.com>
2025-06-03 03:37:39 +0900

    design update

--- main-main/app.py
+++ main-main/app.py
@@ -2,13 +2,14 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
-# UI 파일 경로
+# UI 파일(app.ui) 경로
 ui_file = '/Users/gimhuidong/RAG_project/app.ui'
 
-class MyWindow(QMainWindow):
+class MyWindow(QMainWindow): # 화면을 띄우는데 사용되는 class선언
     def __init__(self):
         super().__init__()
-        uic.loadUi(ui_file, self)  # .ui 파일 로드
+        uic.loadUi(ui_file, self)  # .ui 파일 로드 # datetimeEdit 위젯을 버튼처럼 클릭 이벤트 연결
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
